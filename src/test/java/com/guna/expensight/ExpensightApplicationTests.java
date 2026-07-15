811a3092dd479c405cf4fc751066833710f852d2
guna2007 <lankalapalligunapersonal@gmail.com>
2026-07-15 09:54:22 +0530

    chore: initialize Spring Boot project with Spring Initializr

---
+++ src/test/java/com/guna/expensight/ExpensightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guna.expensight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
